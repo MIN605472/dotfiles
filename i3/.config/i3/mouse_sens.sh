87e66a8726a526c73d0d1fcc59adace550c10b87
MIN605472 <MIN605472@users.noreply.github.com>
2019-10-18 13:52:08 +0200

    Update.

--- i3/.config/i3/mouse_sens.sh
+++ i3/.config/i3/mouse_sens.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-DEVICE_ID=9
+DEVICE_ID="$(xinput | grep -i 'mouse' | sed 's/.*id=\([0-9]\+\).*/\1/')"
 DEFAULT_MAT='1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 1.000000'
-# MY_MAT='0.05 0.000000, 0.000000, 0.000000, 0.05, 0.000000, 0.000000, 0.000000, 1.000000'
-MY_MAT='0.05 0.000000, 0.000000, 0.000000, 0.05, 0.000000, 0.000000, 0.000000, 1.000000'
-PROP_MAT="$(xinput list-props 9 | egrep -i 'Transformation Matrix' | sed -n -e 's/.*Matrix (\([0-9]\+\)):\s*\(.*\)/\1;\2/p')"
+MY_MAT='0.05, 0.000000, 0.000000, 0.000000, 0.05, 0.000000, 0.000000, 0.000000, 1.000000'
+PROP_MAT="$(xinput list-props 12 | egrep -i 'Transformation Matrix' | sed -n -e 's/.*Matrix (\([0-9]\+\)):\s*\(.*\)/\1;\2/p')"
 PROP_ID="$(echo $PROP_MAT | cut -d';' -f1)"
 CURRENT_MAT=$(echo $PROP_MAT | cut -d';' -f2)
 if [[ "$CURRENT_MAT" == "$DEFAULT_MAT"  ]]; then
